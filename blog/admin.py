5550301e011da76eb32f2434dd8bd956f5736dfd
javad <javad.feyzabadi@yahoo.com>
2023-01-25 13:42:28 +0330

    create actions for admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,25 @@
 from django.contrib import admin
 from . models import Article,Category
 
+#create actions 
+def make_published(modeladmin,request,queryset):
+    row_updated = queryset.update(status ='P')
+    if  row_updated == 1 :
+        message_bit = '1 article was'
+    else:
+        message_bit = '%s article were' % row_updated
+    modeladmin.message_user(request,'%s succesfuly marked as published')
+make_published.short_description = 'mark selected article as published'
+
+def make_draft(modeladmin,request,queryset):
+    row_updated = queryset.update(status = 'D')
+    if  row_updated == 1 :
+        message_bit = '1 article was'
+    else:
+        message_bit = '%s article were' % row_updated
+    modeladmin.message_user(request,'%s succesfuly marked as drafted')
+make_draft.short_description = 'mark selected article as drafted'
+
 class CategoryAdmin(admin.ModelAdmin):
     list_display=("position","title","slug","status","parent")
     list_filter =(["status"])
@@ -15,6 +34,7 @@ class ArticleAdmin(admin.ModelAdmin):
     search_fields =('title','description')
     prepopulated_fields={'slug':('title',)}
     ordering = ["status","-publish"]
+    actions = [make_published,make_draft]
 
     def get_category(self,obj):
         return ", ".join([category.title for category in obj.categorey_published()])
