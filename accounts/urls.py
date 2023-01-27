fcdd5289818f85297194ed89d1f775d31789d76e
javad <javad.feyzabadi@yahoo.com>
2023-01-27 23:02:29 +0330

    create views

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,8 @@
 from django.contrib.auth import views
 from django.urls import path
 
+from . views import home,ArticleList
+
 urlpatterns = [
     path("login/", views.LoginView.as_view(), name="login"),
     # path("logout/", views.LogoutView.as_view(), name="logout"),
@@ -28,4 +30,9 @@ urlpatterns = [
     #     views.PasswordResetCompleteView.as_view(),
     #     name="password_reset_complete",
     # ),
+]
+
+
+urlpatterns += [
+    path('',ArticleList.as_view(),name='home'),
 ]
