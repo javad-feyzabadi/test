b0b2638cc9335d7cfdded53a326692fff21905ff
javad <javad.feyzabadi@yahoo.com>
2023-01-29 15:21:21 +0330

    add abstractuser

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.utils import timezone
-from django.contrib.auth.models import User
+from accounts.models import User
 from django.utils.html import format_html
 from django.urls import reverse
 
