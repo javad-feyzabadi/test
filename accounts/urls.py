bf54b6f01fcc8d29364289f9624be1dba19e5f58
javad <javad.feyzabadi@yahoo.com>
2023-01-31 02:27:21 +0330

    edit

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,17 +3,12 @@ from django.urls import path
 
 from . views import (ArticleList,ArticleCreate,
                      ArticleUpdate,ArticleDelete,
-                     Profile,Login,PasswordChange,
+                     Profile
 )
 
 app_name = 'accounts'
 
 urlpatterns = [
-    path("login/",Login.as_view(), name="login"),
-    path("logout/", views.LogoutView.as_view(), name="logout"),
-    path("password_change/", PasswordChange.as_view(), name="password_change"),
-    path("password_change/done/",views.PasswordChangeDoneView.as_view(),name="password_change_done",),
-   
 
     path("",ArticleList.as_view(),name='homee'),
     path("create/",ArticleCreate.as_view(),name='article-create'),
