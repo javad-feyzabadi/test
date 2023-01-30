6c51337dd3f3e0e8ca9342949b14720fc25ec65e
javad <javad.feyzabadi@yahoo.com>
2023-01-31 02:27:10 +0330

    edit models.py

--- accounts/models.py
+++ accounts/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
+    email = models.EmailField(unique=True)
     is_author = models.BooleanField(default=False)
     special_user = models.DateTimeField(default=timezone.now)
 
