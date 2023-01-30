4b62fc24e6bf9cfdcff053405c1b81038869d8bd
javad <javad.feyzabadi@yahoo.com>
2023-01-30 23:31:06 +0330

    edit mixins

--- accounts/mixins.py
+++ accounts/mixins.py
@@ -52,7 +52,10 @@ class SuperUserMixin():
 
 class AuthorsAccessMixin():
     def dispatch(self, request, *args, **kwargs):
-        if request.user.is_superuser or request.user.is_author:
-            return super().dispatch(request, *args, **kwargs)
+        if request.user.is_authenticated:
+            if request.user.is_superuser or request.user.is_author:
+                return super().dispatch(request, *args, **kwargs)
+            else:
+                return redirect('accounts:profile')
         else:
-            return redirect('accounts:profile')
+                return redirect('accounts:login')
