4208233c9cddb0c07717484d692c94398783a75c
javad <javad.feyzabadi@yahoo.com>
2023-01-31 21:26:33 +0330

    srar rating

--- config/urls.py
+++ config/urls.py
@@ -30,6 +30,8 @@ urlpatterns = [
     # re_path(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',activate, name='activate'),
     path('activate/<uidb64>/<token>/',activate, name='activate'),
     path('comment/', include('comment.urls')),
+    path('ratings/', include('star_ratings.urls', namespace='ratings')),
+
 ]
 urlpatterns += static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL,document_root = settings.STATIC_ROOT)
