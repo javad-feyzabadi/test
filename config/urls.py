bc8a24f56b1bca76e92477fe25001b956f58a677
javad <javad.feyzabadi@yahoo.com>
2023-01-31 14:52:32 +0330

    edit path activate

--- config/urls.py
+++ config/urls.py
@@ -27,9 +27,8 @@ urlpatterns = [
     path('accounts/',include('accounts.urls')),
     path("login/",Login.as_view(), name="login"),
     path("register/",Register.as_view(), name="register"),
-    re_path(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',activate, name='activate'),
-
+    # re_path(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',activate, name='activate'),
+    path('activate/<uidb64>/<token>/',activate, name='activate')
 ]
-
 urlpatterns += static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL,document_root = settings.STATIC_ROOT)
