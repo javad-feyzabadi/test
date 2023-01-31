b49c0891eb911f56a3028357f5548c7919283e06
javad <javad.feyzabadi@yahoo.com>
2023-01-31 13:10:55 +0330

    create form for register

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
 
 from . models import User
 
@@ -21,3 +22,10 @@ class ProfileForm(forms.ModelForm):
         fields = ["username","email","first_name","last_name",
 			  "special_user","is_author"
 	    ]
+
+
+class SignupForm(UserCreationForm):
+    email = forms.EmailField(max_length=200)
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
