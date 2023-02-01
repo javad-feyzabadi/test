9ffd20cba36b6546237e9cca477b473fb434ef93
javad <javad.feyzabadi@yahoo.com>
2023-02-01 14:03:49 +0330

    create ArticleHit

--- blog/models.py
+++ blog/models.py
@@ -61,7 +61,7 @@ class Article (models.Model):
     status = models.CharField(max_length=1,choices=STATUS_CHOICES)
     is_special = models.BooleanField(default=False)
     comments = GenericRelation(Comment)
-    hits = models.ManyToManyField(IPAddress,blank=True,related_name="hits")
+    hits = models.ManyToManyField(IPAddress,through='ArticleHit' ,blank=True,related_name="hits")
 
     class Meta:
         ordering = ['-publish']
@@ -82,3 +82,7 @@ class Article (models.Model):
     objects = ArticleManager()
 
 
+class ArticleHit(models.Model):
+    article = models.ForeignKey(Article,on_delete=models.CASCADE)
+    ip_address = models.ForeignKey(IPAddress,on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
