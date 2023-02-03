48b6bd817b33cb4699a54149d535d48d9bc41b7c
javad <javad.feyzabadi@yahoo.com>
2023-02-03 21:56:23 +0330

    edit

--- accounts/views.py
+++ accounts/views.py
@@ -115,12 +115,3 @@ def activate(request, uidb64, token):
 		return HttpResponse('Thank you for your email confirmation. Now you can login your account.<a href="/login">Login</a>')
 	else:
 		return HttpResponse('Activation link is invalid! <a href="/register">Register</a>')
-
-
-
-
-
-
-
-
-   
