7c8cdd24a1d795915519f630d85735026ba15c7d
javad <javad.feyzabadi@yahoo.com>
2023-02-03 21:56:45 +0330

    edit

--- accounts/urls.py
+++ accounts/urls.py
@@ -16,23 +16,6 @@ urlpatterns = [
     path("delete/<int:pk>",ArticleDelete.as_view(),name='article-delete'),
     path("profile/",Profile.as_view(),name='profile'),
 
-    
-    # path("password_reset/", views.PasswordResetView.as_view(), name="password_reset"),
-    # path(
-    #     "password_reset/done/",
-    #     views.PasswordResetDoneView.as_view(),
-    #     name="password_reset_done",
-    # ),
-    # path(
-    #     "reset/<uidb64>/<token>/",
-    #     views.PasswordResetConfirmView.as_view(),
-    #     name="password_reset_confirm",
-    # ),
-    # path(
-    #     "reset/done/",
-    #     views.PasswordResetCompleteView.as_view(),
-    #     name="password_reset_complete",
-    # ),
 ]
 
 
