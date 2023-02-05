a12a693167ccf31cbde541a2674d5810ad34bbb3
javad <javad.feyzabadi@yahoo.com>
2023-02-05 06:22:25 +0330

    edit about

--- config/settings.py
+++ config/settings.py
@@ -134,7 +134,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
-
+# STATICFILES_DIRS = [BASE_DIR / "static"]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
