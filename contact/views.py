635f180c0de16427c0fa409e728876d9eb704968
javad <javad.feyzabadi@yahoo.com>
2023-02-05 03:55:33 +0330

    start contact form  page

--- contact/views.py
+++ contact/views.py
@@ -1,6 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
-# Create your views here.
+from . forms import ContactForm
 
+@login_required
 def contact(request):
-    return render(request,'contact/contact.html')
+    form = ContactForm
+    if request.method == 'POST':
+        form = ContactForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request,'Your message has been received')
+        else:
+            form = ContactForm()
+            messages.error(request,'Your message was not received. Check the input information')
+        return redirect('blog:home')
+    context ={
+        'form':form
+    }
+    return render(request,'contact/contact.html',context)
+
+
+
+
