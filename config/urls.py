a4868c85f87326a81ce5040d57a53c4edeb1176d
javad <javad.feyzabadi@yahoo.com>
2023-02-06 13:46:34 +0330

    ready

--- config/urls.py
+++ config/urls.py
@@ -15,8 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include,re_path
-from django.conf import settings
-from django.conf.urls.static import static
+# from django.conf import settings
+# from django.conf.urls.static import static
 
 from accounts.views import Login,Register,activate
 
@@ -36,6 +36,6 @@ urlpatterns = [
     path('ratings/', include('star_ratings.urls', namespace='ratings')),
 
 ]
-urlpatterns += static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL,document_root = settings.STATIC_ROOT)
+# urlpatterns += static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
+# urlpatterns += static(settings.STATIC_URL,document_root = settings.STATIC_ROOT)
 
